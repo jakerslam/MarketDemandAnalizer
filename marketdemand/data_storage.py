42e0e5a7c76af9214340f1b078774439c91dba25
Jay Haslam <jakerslam@gmail.com>
2025-11-05 02:15:40 -0700

    add caching

---
+++ marketdemand/data_storage.py
@@ -0,0 +1,12 @@
+def get_cached(key):
+    # return cached value or None
+    pass
+
+def set_cache(key, value):
+    # write to file
+    pass
+
+def fetch_or_cache(key, api_call_function):
+    # if cached return it
+    # else call API, store, return result
+    pass
